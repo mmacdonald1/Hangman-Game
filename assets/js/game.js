1ad874edc58bfc106a954ed3bcdba2f4709cbd94
Melanie MacDonald <melmacd94@gmail.com>
2017-10-26 22:56:50 -0400

    ninth

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,14 +3,9 @@ var dogName = [];
 var randomDog;
 var userGuess;
 var wrongArray = [];
-var indexValue;
-var match;
 var dogLengthArray = [];
 var letterIndexArray= [];
-var letterIndex;
-var currentLetter;
-var indices = [];
-var idx;
+    
 
 
 
@@ -23,7 +18,7 @@ var remaining = document.getElementById("remaining");
 var remainingCount = 10;
 var disword = document.getElementById("dis-word");
 var disguess = document.getElementById("dis-guess");
-var resetButton = document.getElementById("reset-button");
+
 
 
 // Dog Array================================================
@@ -58,12 +53,13 @@ var game = {
            
 
             
-    answerArray: function(){      
+    answerArray: function(){ 
+        dogLengthArray=[];
      for (let i = 0; i < game.letterArray().length; i++ ) {
 
          dogLengthArray.push("_ ");
      }
-//    console.log("WOOO", dogLengthArray);
+    console.log("WOOO", dogLengthArray);
     disword.textContent = dogLengthArray;
     },
     
@@ -76,101 +72,75 @@ var game = {
           var userGuess = event.key.toUpperCase(); 
             console.log(userGuess);
             
-             
-            // if(aaray.indexOf(guess) != -1){logic if it does
-            
-            var idx = game.letterArray().indexOf(userGuess);
-            console.log(idx);
-            
-           
-            
-            while (idx != -1){
-               indices.push(idx);
-               idx = game.letterArray().indexOf(userGuess, idx +1);
-                    }
-            
+
             
             if (game.letterArray().includes(userGuess)){
                 console.log('correct')
-                for(var i = 0; i < game.letterArray().length; i++){
                 
+                for(var i = 0; i < game.letterArray().length; i++){
+                if(userGuess === game.letterArray()[i]){
+                   dogLengthArray[i] = userGuess;
+                }
                 disword.textContent = dogLengthArray;
                 console.log(dogLengthArray);
                 }
-           
-            else if (wrongArray.includes(userGuess) === false){
+            }
+            else if (wrongArray.includes(userGuess)) {
+			}
+            else{
                  console.log('incorrect')
                 wrongArray.push(userGuess);
                 disguess.textContent= wrongArray;
                 remaining.textContent = --remainingCount;
             }
-            console.log(indices);
-            console.log(dogLengthArray);
+              if (remainingCount === 0){
+            alert("GAME OVER");
+            game.reset();
+            game.new();
+            }
+             if ( dogLengthArray.join('') === randomDog){
+            score.textContent= ++winCount;
+            alert("YOU WIN!");
+            game.reset();
+            game.new();
+            } 
+            console.log(dogLengthArray.join(""));
+            console.log(randomDog);
         }
+
             },
             
-            
-            // Finding all the occurrences of an element
-            // for loop for indicies to alter the game to show word
-            // }else{ logic for doesn't exist
-            // 
+        reset: function(){ 
+        var dogName = [];
+        var randomDog;
+        var userGuess;
+        var wrongArray = [];
+        var dogLengthArray = [];
+        var letterIndexArray= [];
+        var remainingCount = 10;
+        disword.textContent = "no word";
+        remainingCount= 10;
+        remaining.textContent = remainingCount;
+        disguess.textContent= wrongArray;
+         
+    },
+    
+     new: function(){
+            game.dogName();
+
+            game.letterArray();
+
+            game.answerArray();
+
+            game.handlesKeyPress();
+        },
         };
             
-//         for (let i = 0; i < game.letterArray().length; i++ ) {
-//             currentLetter = game.letterArray()[i];
-//            console.log(currentLetter === userGuess)
-//             
-//             
-//             
-//           if (currentLetter === userGuess) {
-//               console.log('correct')
-//                dogLengthArray[i]=currentLetter;
-//                disword.textContent = dogLengthArray;
-//               return dogLengthArray;
-//            } 
-//            if (currentLetter !== userGuess) {
-//               console.log('incorrect')
-//             
-//                if(wrongArray.includes(userGuess) === false){
-//                console.log('Not a matching char bruh, and your character is not in the aray.')
-//                wrongArray.push(userGuess);
-//                disguess.textContent= wrongArray;
-//                remaining.textContent = --remainingCount;
-//                }   
-//            }
-//         }
-//            console.log('Wrong ARAAY #############', wrongArray);
-//            console.log(dogLengthArray);
-////            return dogLengthArray;    
-//         
-//        }
-
-             
-//            else if (wrongArray.includes(userGuess)){}
-//            
-//            else{
-//                wrongArray.push(userGuess);
-//                console.log(wrongArray);
-//                disguess.textContent= wrongArray;
-//                remaining.textContent = --remainingCount;
-//            }
-//            console.log(wrongArray);
-//            return wrongArray;
-//         }
-//        if (remainingCount === 0){
-//            alert("GAME OVER");
-//        }
+   
 //        
-//        if ( dogLengthArray.join === randomDog){
-//            score.textContent= ++scoreCount;
-//        } 
+       
     
-//    reset: function(){
-//      document.addEventListener("click");  
-//        game.dogName();
-//        remainingCount= 10;
-//        wrongArray =[];
-//    },
+   
 
 
 
@@ -187,6 +157,6 @@ game.answerArray();
 
 game.handlesKeyPress();
 
-//game.document.onkeyup();
+
 
 
